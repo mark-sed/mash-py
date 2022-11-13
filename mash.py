a07917bd04491b30ceaf049219f8752a7a7ec8b9
Marek Sedlacek <mr.mareksedlacek@gmail.com>
2022-11-13 16:57:10 +0100

    v0.3.0

--- mash.py
+++ mash.py
@@ -4,7 +4,7 @@ Mash interpreter
 """
 __author__ = "Marek Sedlacek"
 __date__ = "September 2022"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __email__ = "mr.mareksedlacek@gmail.com"
 
 import argparse
